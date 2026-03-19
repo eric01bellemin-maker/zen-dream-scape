bc097ebeba0ec5df3f245245a76385c1c0eab5b5
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-19 23:35:19 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,1 @@
-function App() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Sonora Zen</h1>
-      <p>Le site est en cours de restauration.</p>
-    </div>
-  );
-}
 
-export default App;
