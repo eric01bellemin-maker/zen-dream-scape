f2ac2438ecc1d7ef41dac33a0f3db2ba55cd6d74
eric01bellemin-maker <eric01bellemin@gmail.com>
2026-03-19 22:54:34 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,1 @@
-const Index = () => {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
-      <h1>ZenDream</h1>
-      <p>Le site est en cours de restauration.</p>
-    </main>
-  );
-};
 
-export default Index;
